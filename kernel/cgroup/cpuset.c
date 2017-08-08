91152efde80d3b8d3208596b047140af25d2ddb0
Peter Zijlstra <peterz@infradead.org>
2017-08-08 12:16:24 +0200

    sched/topology, cpuset: Avoid spurious/wrong domain rebuilds
    When disabling cpuset.sched_load_balance we expect to be able to online
    CPUs without generating sched_domains. However this is currently
    completely broken.
    
    What happens is that we generate the sched_domains and then destroy
    them. This is because of the spurious 'default' domain build in
    cpuset_update_active_cpus(). That builds a single machine wide domain
    and then schedules a work to build the 'real' domains. The work then
    finds there are _no_ domains and destroys the lot again.
    
    Furthermore, if there actually were cpusets, building the machine wide
    domain is actively wrong, because it would allow tasks to 'escape' their
    cpuset. Also I don't think its needed, the scheduler really should
    respect the active mask.
    
    Reported-by: Ofer Levi(SW) <oferle@mellanox.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vineet.Gupta1@synopsys.com <Vineet.Gupta1@synopsys.com>
    Cc: rusty@rustcorp.com.au <rusty@rustcorp.com.au>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2462,13 +2462,10 @@ static void cpuset_hotplug_workfn(struct work_struct *work)
 void cpuset_update_active_cpus(void)
 {
 	/*
-	 *
-	 * We still need to do partition_sched_domains() synchronously;
-	 * otherwise, the scheduler will get confused and put tasks to the
-	 * dead CPU.  Fall back to the default single domain.
-	 * cpuset_hotplug_workfn() will rebuild it as necessary.
+	 * We're inside cpu hotplug critical region which usually nests
+	 * inside cgroup synchronization.  Bounce actual hotplug processing
+	 * to a work item to avoid reverse locking order.
 	 */
-	partition_sched_domains(1, NULL, NULL);
 	schedule_work(&cpuset_hotplug_work);
 }
 
